1baf6e980c4481f60b9ae54bc6266e6d30fcaee3
Yafet Nega <yafet@Yafets-MacBook-Pro.local>
2020-10-30 14:00:40 +0300

    Subtraction function is added

--- sketch.test.js
+++ sketch.test.js
@@ -1,5 +1,5 @@
 
-const {sum, prod, div}  = require('./sketch');
+const {sum, prod, div, sub}  = require('./sketch');
 const { test, expect } = require('@jest/globals');
 
 test('add 1 + 2 equals 3', () => {
@@ -12,4 +12,8 @@ test('multiply 3 * 10 equals 30', () => {
 
 test('divide 30 / 3 equals 10', () => {
     expect(div(30, 3)).toBe(10);
+});
+
+test('subtruction 30 - 10 equals 20', () => {
+    expect(sub(30, 10)).toBe(20);
 });
