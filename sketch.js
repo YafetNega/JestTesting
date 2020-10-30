d31c27cdcb68af7ddf8ede852d5d9341f4b96e54
Yafet Nega <yafet@Yafets-MacBook-Pro.local>
2020-10-30 13:54:57 +0300

    Division function is added

--- sketch.js
+++ sketch.js
@@ -6,7 +6,12 @@ function prod(a, b){
     return a * b;
 }
 
+function div(a, b){
+    return a / b;
+}
+
 module.exports = {
     sum: sum,
     prod: prod,
+    div: div,
 };
--- sketch.test.js
+++ sketch.test.js
@@ -1,5 +1,5 @@
 
-const {sum, prod}  = require('./sketch');
+const {sum, prod, div}  = require('./sketch');
 const { test, expect } = require('@jest/globals');
 
 test('add 1 + 2 equals 3', () => {
@@ -8,4 +8,8 @@ test('add 1 + 2 equals 3', () => {
 
 test('multiply 3 * 10 equals 30', () => {
     expect(prod(3, 10)).toBe(30);
+});
+
+test('divide 30 / 3 equals 10', () => {
+    expect(div(30, 3)).toBe(10);
 });
