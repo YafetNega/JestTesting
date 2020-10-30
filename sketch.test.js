ad6a9722bef16caab3a587ae5fb2687820e91fb4
Yafet Nega <yafet@Yafets-MacBook-Pro.local>
2020-10-30 13:42:19 +0300

    Multiplication function added

--- sketch.test.js
+++ sketch.test.js
@@ -1,7 +1,11 @@
 
-const sum = require('./sketch');
+const {sum, prod}  = require('./sketch');
 const { test, expect } = require('@jest/globals');
 
 test('add 1 + 2 equals 3', () => {
     expect(sum(1, 2)).toBe(3);
+});
+
+test('multiply 3 * 10 equals 30', () => {
+    expect(prod(3, 10)).toBe(30);
 });
